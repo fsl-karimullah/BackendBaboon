3362be8e8008d5d8802da31d91903ab920d97964
fsl-karimullah <faisalbic123@gmail.com>
2023-05-05 17:21:19 +0700

    Add bookmark status in book details

--- app/Http/Resources/Books/BookResource.php
+++ app/Http/Resources/Books/BookResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Books;
 
+use App\Models\Bookmark;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class BookResource extends JsonResource
@@ -18,6 +19,8 @@ class BookResource extends JsonResource
             'publisher' => $this->publisher,
             'publishedDate' => $this->published_date,
             'pageCount' => $this->page_count,
+            'is_bookmarked' => Bookmark::where('book_id', $this->id)
+                ->where('user_id', auth()->user()->id)->count() ? true : false,
             'thumbnail' => $this->thumbnail ? asset('/storage/'.$this->thumbnail) : null,
             'pdf_preview' => $this->pdf_file_preview ? asset('/storage/'.$this->pdf_file_preview) : null,
             'pdf_full' => $this->pdf_file ? asset('/storage/'.$this->pdf_file) : null,
